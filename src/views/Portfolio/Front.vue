f71325960f21ae8e16c345ad6a6480f47894c667
stevy <stevyralambomanana@gmail.com>
2023-10-08 16:26:06 +0200

    font

--- src/views/Portfolio/Front.vue
+++ src/views/Portfolio/Front.vue
@@ -31,10 +31,13 @@
               <h3 class="headline mb-0">
                 <span>Technology</span>
               </h3>
-              <v-chip color="green" text-color="white">{{project.tech.tech1}}</v-chip>
-              <v-chip color="green" text-color="white">{{project.tech.tech2}}</v-chip>
-              <v-chip color="green" text-color="white">{{project.tech.tech3}}</v-chip>
-              <v-chip color="green" text-color="white">{{project.tech.tech4}}</v-chip>
+              <v-chip color="purple" v-for="techno in project.tech" text-color="white">{{techno}}</v-chip>
+            </v-card-text>
+            <v-card-text>
+              <h3 class="headline mb-0">
+                <span>Description</span>
+              </h3>
+              <div  v-for="description in project.description" text-color="white">{{description}}</div>
             </v-card-text>
             <v-card-actions>
               <v-btn flat large dark color="green" :href="project.git" target="_blank">
@@ -84,121 +87,101 @@
         projects: [
            {
             dialog: false,
-            title: "Euphoric Voyage",
+            title: "Sneat",
             git: "https://github.com/EldinZaimovic/euphoric-voyage",
             demo: "https://www.euphoric-voyage.com/",
-            tech: {
-              tech1: "Gatsby",
-              tech2: "JavaScript",
-              tech3: "SCSS",
-              tech4: "Markdown"
-            },
-            poster: "https://i.imgur.com/9Ww2bvm.png"
+            tech: [
+              "Javascript",
+              "VueJs 3",
+              "VueX",
+              "Vuetify"
+            ],
+            poster: "https://i.imgur.com/9Ww2bvm.png",
+            description:[
+                "Application Reseau Socio",
+                "amis / actualite / commentaire / reaction / profile / groupe"
+            ]
           },
           {
             dialog: false,
-            title: "This Site",
-            git: "https://github.com/EldinZaimovic/Eldin-Space-Vue",
-            demo: "https://eldin.space/",
-            tech: {
-              tech1: "VUE",
-              tech2: "Storyblok",
-              tech3: "HTML",
-              tech4: "JavaScript"
-            },
-            poster: "https://i.imgur.com/sGUofpv.png"
+            title: "Lugx_gaming",
+            git: "https://github.com/stevy-sys/lugx_gaming.git",
+            demo: "https://lugx.rubycorp.fr/api/documentation",
+            tech: [
+              "Angular",
+            ],
+            poster: "/port/lugx.JPG",
+            description:[
+                "API e-commerce Gaming ",
+                "produit / cart / panier / payment"
+            ]
           },
           {
             dialog: false,
-            title: "Frontend Developer at Brandly.com",
-            git: "https://cdn.neow.in/news/images/uploaded/2018/11/1543476286_cybersecurity.jpg",
-            demo: "https://www.brandly.com/",
-            tech: {
-              tech1: "VUE",
-              tech2: "SCSS",
-              tech3: "HTML",
-              tech4: "JavaScript"
-            },
-            poster: "https://i.imgur.com/nVqtTAf.png"
+            title: "Chat Apps",
+            git: "https://github.com/stevy-sys/chat-api.git",
+            demo: "https://chat.rubycorp.fr/documentation",
+            tech: [
+              "VueJS 3",
+              "Laravel-echo ",
+              "VueX",
+              "pusherJS",
+            ],
+            poster: "/port/chatapi.JPG",
+            description:[
+                "Chat message user ",
+                "conversation groupe / conversation prive / message / presence en ligne / temp reelle"
+            ]
           },
           {
             dialog: false,
-            title: "Digital Madness Test",
-            git: "https://github.com/EldinZaimovic/Digital-Madness-test",
-            demo: "https://digital-madness-test.netlify.com/",
-            tech: {
-              tech1: "VUE",
-              tech2: "CSS",
-              tech3: "HTML",
-              tech4: "JavaScript"
-            },
-            poster: "https://i.imgur.com/GOIIL06.png"
-          },
-          {
-            dialog: false,
-            title: "Snowball Coding Challenge",
-            git: "https://github.com/EldinZaimovic/SnowBall-Project",
-            demo: "https://snowball-test.netlify.com/",
-            tech: {
-              tech1: "VUE",
-              tech2: "GraphQL",
-              tech3: "Axios",
-              tech4: "JavaScript"
-            },
-            poster: "https://i.imgur.com/rXHBs36.png"
-          },
-          {
-            dialog: false,
-            title: "IBM Coding Challenge - Films Location",
-            git: "https://github.com/EldinZaimovic/Film-Location-List",
-            demo: "https://film-location-ibm-cc.netlify.com/",
-            tech: {
-              tech1: "VUE",
-              tech2: "Lodash",
-              tech3: "HTML",
-              tech4: "JavaScript"
-            },
-            poster: "https://i.imgur.com/kCN1CFH.png"
-          },
-          {
-            dialog: false,
-            title: "Stock Trader",
-            git: "https://github.com/EldinZaimovic/Stock-Trader",
-            demo: "https://stock-trader-eldin.netlify.com/",
-            tech: {
-              tech1: "VUE",
-              tech2: "Vuex",
-              tech3: "Firebase",
-              tech4: "JavaScript"
-            },
-            poster: "https://i.imgur.com/wK5dqP7.png"
-          },
-          {
-            dialog: false,
-            title: "Riders Share Newsleter",
+            title: "Noah Voyage",
             git: "",
-            demo: "https://imgur.com/zTMJhGk",
-            tech: {
-              tech1: "HTML",
-              tech2: "CSS",
-              tech3: "MailChimp",
-              tech4: "JavaScript"
-            },
-            poster: "https://i.imgur.com/zTMJhGk.jpg"
+            demo: "https://noah-voyages.com/",
+            tech: [
+              "Vue2",
+              "VueX",
+              "laravel-echo",
+              "pusherJS",
+            ],
+            poster: "/port/noah.JPG",
+            description:[
+                "Apps d'Agence de voyage ",
+                "circuit / customisation / reservation / paiment / "
+            ]
           },
           {
             dialog: false,
-            title: "My Old Web Portfolio",
-            git: "https://github.com/EldinZaimovic/EldinZaimovic.github.io",
-            demo: "https://eldinzaimovic.github.io/index.html",
-            tech: {
-              tech1: "HTML",
-              tech2: "CSS",
-              tech3: "JavaScript",
-              tech4: "jQuery"
-            },
-            poster: "https://i.imgur.com/AW7CXD5.jpg"
-          }
+            title: "ShareMyHobbies",
+            git: "",
+            demo: "https://noah-voyages.com/",
+            tech: [
+              "Vue2",
+              "VueX",
+              "laravel-echo",
+              "pusherJS",
+            ],
+            poster: "/port/noah.JPG",
+            description:[
+                "Apps Reseau socio pour les Hobbies ",
+                "amis / actualite / commentaire / reaction / profile / groupe / chat / evenement / parametre / appel-video"
+            ]
+          },
+          {
+            dialog: false,
+            title: "Documentation Stevy/Framwork ",
+            git: "https://github.com/stevy-sys/mon-framework.git",
+            demo: "https://github.com/stevy-sys/mon-framework.git",
+            tech: [
+              "React",
+              "Material UI"
+            ],
+            poster: "",
+            description:[
+                "Documentation de Stevy/Framework",
+                "Controller / Model / Migration / Vue / Commande console"
+            ]
+          },
         ]
       };
     }
